24a25628f09286a2fb74321476f87aaad12eb8fe
Elias Bauer <91609312+EliasBauer@users.noreply.github.com>
2024-01-26 18:22:19 +0000

    set up project and apps

---
+++ storefront/store/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
